cfda80377e3b036c9e4825785ef9ac024c8730e4
nurul <nurulkhoiyah86@gmail.com>
2024-03-14 01:07:44 +0700

    Akhirnya ketemu

--- src/oi/Oi.java
+++ src/oi/Oi.java
@@ -16,6 +16,7 @@ public class Oi {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Oiya");
+        System.out.println("Akhirnya ketemu");
     }
     
 }
